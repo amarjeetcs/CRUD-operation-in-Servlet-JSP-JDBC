7a6cb3e42836e12e3cb80f4372403b0dddb007c4
amarjeet <amarjeetcs79@gmail.com>
2021-07-29 11:26:35 +0530

    All Commit

---
+++ UserApp/src/com/amarjeetcs/createServlert/UpdateUserServlet.java
@@ -0,0 +1,81 @@
+package com.amarjeetcs.createServlert;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class CreateUserServlet
+ */
+@WebServlet("/updateServlet")
+public class UpdateUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Connection connection;
+	
+       
+	public void init(ServletConfig config)
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection=DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","root");
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		
+		try {
+		Statement statement=connection.createStatement();
+		int result=statement.executeUpdate("update user set password='"+password+"' where email='"+email+"'");
+		PrintWriter out=response.getWriter();
+		if(result>0)
+		{
+			out.print("<h1>PASSWORD UPDATED</h>");
+		}
+		else
+		{
+			out.print("<h1>INVALID USER</h>");
+		}
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void destroy()
+	{
+		try {
+			connection.close();
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
